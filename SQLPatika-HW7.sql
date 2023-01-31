0877677fd077392cdb4442aa48797b5c7f21eb27
ihsancakmak <95715853+ihsancakmak@users.noreply.github.com>
2023-01-31 18:39:30 +0300

    Add files via upload

---
+++ SQLPatika-HW7.sql
@@ -0,0 +1,18 @@
+--Q1-film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+SELECT rating FROM film
+GROUP BY rating;
+
+--Q2-film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+SELECT replacement_cost, COUNT(replacement_cost) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(replacement_cost) > 50;
+
+--Q3-customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
+
+--Q4-city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT country_id, COUNT(city_id) FROM city
+GROUP BY country_id
+ORDER BY COUNT(city_id) DESC
+LIMIT 1;
